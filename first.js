2fdbf74d3c98b2ed8f8116a61b2fc691f86210ba
Deepak <deepakrajawat223@gmail.com>
2025-11-06 16:41:32 -0800

    upi updated after payment upi added after payment

--- first.js
+++ first.js
@@ -8,3 +8,6 @@ const footer = "footer added in our website";
 console.log(footer);
 const payment = "Inegreatet the payment getway";
 console.log(payment);
+
+const upi = "intigrated the upi also";
+console.log(upi);
